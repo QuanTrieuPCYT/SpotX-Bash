2fb34a39452929d7e1fb64786993efa3346d0fa1
QuanTrieuPCYT <admin@qtpc.tech>
2026-03-15 15:40:11 +0700

    change build ver to latest on stash

--- spotx.sh
+++ spotx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-buildVer="1.2.85.519.g549a528b"
+buildVer="1.2.81.264.gb4ad4efa-1852"
 
 command -v perl >/dev/null || { echo -e "\n${red}Error:${clr} perl command not found.\nInstall perl on your system then try again.\n" >&2; exit 1; }
 
