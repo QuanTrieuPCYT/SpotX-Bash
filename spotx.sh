1320e12fd0c1f24594bace072b496cbd61f183e4
QuanTrieuPCYT <admin@qtpc.tech>
2026-03-15 23:20:20 +0700

    match link

--- spotx.sh
+++ spotx.sh
@@ -215,7 +215,7 @@ macos_prepare() {
   archVar=$(sysctl -n machdep.cpu.brand_string | grep -q "Apple" && echo "arm64" || echo "x86_64")
   [[ "${debug}" ]] && echo -e "${green}Debug:${clr} ${archVar} detected"
   grab1=$(echo "wgGSkVXTzEWdsdkYm50VZRXOTVWbsdEZ2J0MjZ3ZykFbS5GTqJESkhXNDFmeGdEZ6lTeMZTTINGMShUY" | rev | base64 --decode | base64 --decode)
-  grab2=$(echo "==QP9cnS5IUePlWREpUaBNEZ1xWbjdnQ59EeRNUS5E0QaxGaykFMGdlYrFUeldGOHp1ZZlmSnt2QaxGaykFMGdlYrd2QaxWNXFWbWdkWoFUaK1WQ5xEcwgVTzFkelZjSXZ2dJRET4NnbM5WSTZWeG1mV1lzVhpnSYplM0hkSpN2UMlDbU10N1knSpBjbjhmWGFmaKhVW3IVaJ5GMTZmMJpXZ11EWmJzcuxEMod0S2N2QJxWNXx0Z312YsJESJhjQplUOGpWWop0MadjUpl0Z3BzY0F0UjRXQDJWeWNTW" | rev | base64 --decode | base64 --decode)
+  grab2=$(echo "==gbwgUS3k0UNtWSDlEM1cVY5JESJdTREp0ZwQUSrZ1RhpmUYlFdSNUS3IUeitmQppUbBN1SrZ1RhpmUYlFdSN0SrZVbiBnWXp1aGNUStl1QJZ3aTZmeJRET4NnbM5WSTZWeG1mV1lzVhpnSYplM0hkSpN2UMlDbU10N1knSpBjbjhmWGFmaKhVW3IVaJ5GMTZmMJpXZ11EWmJzcuxEMod0S2N2QJxWNXx0Z312YsJESJhjQplUOGpWWop0MadjUpl0Z3BzY0F0UjRXQDJWeWNTW" | rev | base64 --decode | base64 --decode)
   grab3=""; for i in {1..5}; do grab3=$(eval "${grab2}"); [[ -n "${grab3}" ]] && break || sleep 2; done
   [[ -z "${grab3}" ]] && { echo -e "\n${red}Error:${clr} Unable to retrieve download link\n" >&2; exit 1; }
   fileVar=$(basename "${grab3}")
